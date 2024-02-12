8373530dba51426147a0f0f7f9bfb252e304d74d
burcukilic <94201593+burcukilic@users.noreply.github.com>
2024-02-12 17:13:16 +0100

    docs: added javadoc for GradleHelper

--- src/se/DD2480Group3/assignment2/utils/GradleHelper.java
+++ src/se/DD2480Group3/assignment2/utils/GradleHelper.java
@@ -9,12 +9,19 @@ import org.gradle.tooling.ResultHandler;
 import java.io.File;
 
 
-
+/**
+ * A utility class for building Gradle Projects.
+ */
 public class GradleHelper{
 
     private GradleConnector connector;
     private ProjectConnection connection;
 
+
+    /**
+     * Constructor of GradleHelper class, creates a new connection with the given project.
+     * @param pathToProject The path to the project directory to be built. To test for the current project directory "." must be used.
+     */
     public GradleHelper(String pathToProject){
         connector = GradleConnector.newConnector();
         connector.forProjectDirectory(fileFromPathName(pathToProject));
@@ -23,6 +30,10 @@ public class GradleHelper{
         
     }
 
+    /**
+     * Initiates the build process for the Gradle project and prints it's progress.
+     * Notifies upon completion, prints to the console if the build and tests are successfully completed or writes the cause of exception in case of a failure.
+     */
     public void build(){
         
         connection.newBuild().addProgressListener(new ProgressListener() {
@@ -48,7 +59,11 @@ public class GradleHelper{
     }
 
       
-    
+    /**
+     * Creates a new File from the given path.
+     * @param pathToProject The path to the project directory. 
+     * @return A File object representing the project directory.
+     */
     private File fileFromPathName(String pathToProject){
         return new File(pathToProject);
     }
