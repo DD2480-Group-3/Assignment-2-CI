c1b19a3ec88b45d50e9d49d27ac569faa28b43ac
karlsb <36365664+karlsb@users.noreply.github.com>
2024-02-13 13:49:08 +0100

    Refactor: Removed depricated code in ContinuousIntegrationServer. Closes issue #40
    Remved execute function and routes in the handle function

--- src/se/DD2480Group3/assignment2/ContinuousIntegrationServer.java
+++ src/se/DD2480Group3/assignment2/ContinuousIntegrationServer.java
@@ -1,18 +1,13 @@
 package se.DD2480Group3.assignment2; 
 
 //for process handling
-import java.io.BufferedReader;  
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.InterruptedException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.json.JSONObject;
-import org.json.JSONException;
 
 // import src.se.DD2480Group3.assignment2.EmailService;
 
@@ -39,27 +34,6 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         System.out.println(handler.getBranchName());
         System.out.println(handler.getRepoSshUrl());
         System.out.println(handler.getRepoHttpUrl());
-
-    if (target.equalsIgnoreCase("/compile")) {
-      execute("/home/karl/Documents/GitHub/Assignment-2-CI/scripts/compile.sh");
-      /*Send response*/
-      response.getWriter().println("Compiling code");
-    } else if (target.equalsIgnoreCase("/run")) {
-      execute("/home/karl/Documents/GitHub/Assignment-2-CI/scripts/run.sh");
-      /*Send response*/
-      response.getWriter().println("Starting application");
-    } else if (target.equalsIgnoreCase("/test")) {
-      execute("/home/karl/Documents/GitHub/Assignment-2-CI/scripts/test.sh");
-      /*Send response*/
-      response.getWriter().println("Running tests");
-    } else {
-      response.getWriter().println("Default route, doing nothing");
-    }
-    // here you do all the continuous integration tasks
-    // for example
-    // 1st clone your repository
-    // 2nd compile the code
-
   }
 
   // used to start the CI server in command line
@@ -72,39 +46,5 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     server.start();
     server.join();
   }
-
-  /* Source : https://mkyong.com/java/how-to-execute-shell-command-from-java/ */
-  private void execute(String filepath) {
-    ProcessBuilder compileScript = new ProcessBuilder();
-    compileScript.command(filepath);
-
-    try {
-      Process process = compileScript.start();
-
-      StringBuilder output = new StringBuilder();
-
-      BufferedReader reader = new BufferedReader(
-        new InputStreamReader(process.getInputStream())
-      );
-
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-      //What is exitval?
-      int exitVal = process.waitFor();
-      if (exitVal == 0) {
-        System.out.println("success!");
-        System.out.println(output);
-      } else {
-        //?
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
-
 
 }
