eacacc8b7818396f87dfc8bedf1b783880f4b370
karlsb <36365664+karlsb@users.noreply.github.com>
2024-02-13 11:51:12 +0100

    Feat: Constructor for Webhookhandler class issue #35
    Constructor for class that handles webhooks. part of a refactoring issue #35

--- src/se/DD2480Group3/assignment2/WebhookHandler.java
+++ src/se/DD2480Group3/assignment2/WebhookHandler.java
@@ -1,6 +1,31 @@
 package se.DD2480Group3.assignment2;
 
+import org.json.JSONObject;
+import java.io.BufferedReader;  
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+
 class WebhookHandler {
+    
+    HttpServletRequest request;
+    JSONObject payload;
 
+    public WebhookHandler(HttpServletRequest request){
+        this.request = request;
+        this.payload = createPayload();
+    }
 
+    private JSONObject createPayload(){
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader buf = request.getReader();
+            String line;
+            while((line = buf.readLine()) != null) {
+                builder.append(line);
+            }
+        }catch(IOException e){
+            System.err.println("Error reading request" + e.getMessage());
+        }
+        return new JSONObject(builder.toString());
+    }
 }
